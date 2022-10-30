4916b4554910fbc89e1d28cdcdcd8390859aedba
hlzheng-git <52538471+hlzheng-git@users.noreply.github.com>
2022-10-30 18:39:41 +0800

    Update xray.sh

--- xray.sh
+++ xray.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 if [ ! -f UUID ]; then
-  UUID="ad806487-2d26-4636-98b6-ab85cc8521f7"
+  #UUID="ad806487-2d26-4636-98b6-ab85cc8521f7"
+  UUID="ffb03d0f-3e06-4565-ae7a-c38c41246748"
 fi
 
 # Set config.json
